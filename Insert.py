7a9471846c4c9d0e8c340a2fa85b61176be513e9
lucasrocha2704 <lucas.rocha@sptech.school>
2024-10-10 18:21:02 -0300

    Feat: criação de csv com os dados coletados pelo crawler

--- Insert.py
+++ Insert.py
@@ -1,17 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import mysql.connector
+import csv
+import os  
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 con = mysql.connector.connect(
     host='localhost',
     database='saferide',
     user='safeuser',
-    password='eunaosei'
+    password='eunaosei',
+    charset='utf8mb4',
+    collation='utf8mb4_general_ci'
 )
+
 
 def insert_escolas(nome_escola: str, endereco_id: int):
     mySql_insert = "INSERT INTO escola (nome, endereco_id) VALUES (%s, %s)"
@@ -21,7 +26,7 @@ def insert_escolas(nome_escola: str, endereco_id: int):
     cursor.close()
 
 def endereco(cep: str):
-    mySql_insert = "INSERT INTO endereco (cep, usuario_id) VALUES (%s, 32)"
+    mySql_insert = "INSERT INTO endereco (cep, usuario_id) VALUES (%s, 100)"
     cursor = con.cursor()
     cursor.execute(mySql_insert, (cep,))
     con.commit()
@@ -35,10 +40,20 @@ def select_endereco(cep: str):
     cursor.close()
     return resultado[0] if resultado else None
 
+os.remove("enderecos.csv")
+os.remove("escolas.csv")
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless") 
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-driver = webdriver.Chrome(options=chrome_options)
+
+dados_escolas_csv = []
+dados_enderecos_csv = []
+
+
+cabecalho_escolas = ["nome", "endereco_id"]
+cabecalho_enderecos = ["cep", "usuario_id"]
+
 
 for i in range(1, 749):
     url = f'https://pesquisaseduc.fde.sp.gov.br/localize_escola?pageNumber={i}&idMunicipio=100&inicial=False'
@@ -59,11 +74,49 @@ for i in range(1, 749):
 
         print(f"CEP: {cep_escola}")
 
+
         if id_endereco:
             print(f"ID Endereço: {id_endereco}")
             insert_escolas(nome_escola, id_endereco)
+
+            dados_escolas_csv.append([nome_escola, id_endereco])
+            dados_enderecos_csv.append([cep_escola, 100])  
+
         else:
             print(f"Endereço não encontrado para CEP: {cep_escola}")
 
+
+    if len(dados_escolas_csv) >= 200:
+        if not os.path.exists('escolas.csv'):
+            with open('escolas.csv', mode='w', newline='', encoding='utf-8') as file:
+                writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+                writer.writerow(cabecalho_escolas)  
+        with open('escolas.csv', mode='a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+            writer.writerows(dados_escolas_csv)
+        dados_escolas_csv.clear()  
+
+    if len(dados_enderecos_csv) >= 200:
+
+        if not os.path.exists('enderecos.csv'):
+            with open('enderecos.csv', mode='w', newline='', encoding='utf-8') as file:
+                writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+                writer.writerow(cabecalho_enderecos)
+        with open('enderecos.csv', mode='a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+            writer.writerows(dados_enderecos_csv)
+        dados_enderecos_csv.clear()  
+
+if dados_escolas_csv:
+    with open('escolas.csv', mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerows(dados_escolas_csv)
+
+if dados_enderecos_csv:
+    with open('enderecos.csv', mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerows(dados_enderecos_csv)
+
+# Finaliza o driver e a conexão com o banco de dados
 driver.quit()
 con.close()
